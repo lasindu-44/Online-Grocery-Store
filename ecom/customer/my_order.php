b1ef8f752a5b177f9f24e8de3bbdaff54a728b09
Lasindu Dulanjana Jayasiri <68988392+lasindu-44@users.noreply.github.com>
2021-12-07 20:50:33 +0530

    Add my_order.php page
    Add my order table and include dummy values into it.

---
+++ ecom/customer/my_order.php
@@ -0,0 +1,39 @@
+<div class="box"><center>
+    <h1>
+        My Order
+    </h1>
+    <p>
+    If you have any question, please feel free to <a href="../contactus.php">Contact Us</a>, Our
+         Customer Service Center is working for you 24/7.
+    </p>
+</center>
+<hr>
+<div class="table-responsive">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>Sr.No</th>
+                <th>Due Amount</th>
+                <th>Invoice No</th>
+                <th>Quantity</th>
+                <th>Size</th>
+                <th>Order Date</th>
+                <th>Paid/Unpaid</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>#1</td>
+                <td>INR 399</td>
+                <td>554541</td>
+                <td>2</td>
+                <td>Large</td>
+                <td>2019-10-12</td>
+                <td>Unpaid</td>
+                <td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">confirm If Paid</a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</div>
