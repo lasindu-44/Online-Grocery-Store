ab8582ade2ddf1d824003ec60c737aada2347773
Lasindu Dulanjana Jayasiri <68988392+lasindu-44@users.noreply.github.com>
2021-12-07 23:09:59 +0530

    Add Delete Account Page

---
+++ ecom/customer/delete_ac.php
@@ -0,0 +1,11 @@
+<div class="box">
+    <center>
+        <h1>Do You Realy Want To Delete Your Account</h1>
+   
+    
+    <form action="" method="post">
+        <input type="submit" name="yes" value="Yes, I want To Delete" class="btn btn-danger">
+        <input type="submit" name="no"  value="No, I Dont Want" class="btn btn-primary">
+    </form>
+    </center>
+</div>
