76ce74c97028371327f1f58daf9acc16275bcc07
Lasindu Dulanjana Jayasiri <68988392+lasindu-44@users.noreply.github.com>
2021-12-07 22:08:43 +0530

    Add pay offline page

---
+++ ecom/customer/pay_offline.php
@@ -0,0 +1,30 @@
+<div class="box">
+    <center>
+        <h1>Pay Offline Using Bellow Method.</h1>
+        <p>If you have any question, please feel free to <a href="../contactus.php">Contact Us</a>, Our Customer Service Center is working for
+         you 24/7.</p>
+         
+    </center>
+    <hr>
+    <div class ="table-responsive">
+        <table class="table table-bordered tabel-hover tabel-striped">
+            <thead>
+                <tr>
+                <th>Bank Account Detail</th>
+                <th>Paypal Id</th>
+                <th>Payumoney Link</th>
+                <th>PayTm Number</th>
+               </tr>
+
+            </thead>
+            <tbody>
+                <tr>
+                    <td>3197015000927</td>
+                    <td>lasindudulanjana44@gmail.com</td>
+                    <td>xxxxxxxxxxxxxx</td>
+                    <td>95674356728</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
