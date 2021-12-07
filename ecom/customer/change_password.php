9e016a72b8d9d5959271db6953ecc575f6b0f9c0
Lasindu Dulanjana Jayasiri <68988392+lasindu-44@users.noreply.github.com>
2021-12-07 22:56:19 +0530

    Add Change Password Page

---
+++ ecom/customer/change_password.php
@@ -0,0 +1,23 @@
+<div class="box">
+    <center>
+        <h3>Change Your Password</h3>
+
+    </center>
+    <div class="form-group">
+        <label>Enter Your Current Password</label>
+        <input type="password" name="old_password" class="form-control" required="">
+    </div>
+    <div class="form-group">
+        <label>Enter New Password</label>
+        <input type="password" name="new_password" class="form-control" required="">
+    </div>
+    <div class="form-group">
+    <label>confirm New Password</label>
+     <input type="password" name="c_n_password" class="form-control" required="">  
+    </div>
+    <div class="text-center">
+        <button class="btn btn-primary btn-lg">
+            Update Now
+        </button>
+    </div>
+</div>
