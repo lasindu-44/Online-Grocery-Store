71f68e5d218ea112cc575a6fbafae4ed1b59e5da
Lasindu Dulanjana Jayasiri <68988392+lasindu-44@users.noreply.github.com>
2021-12-07 14:46:30 +0530

    Add Customer folder to the Workspace
    In this step, I add the Customer Folder to my htdocs. Inside the Customer Folder, I create the My_account.php page. (only the navbar and footer)

---
+++ ecom/customer/includes/footer.php
@@ -0,0 +1,89 @@
+<div id="footer"><!--Footer Section Start-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 start-->
+                <h4>Page</h4>
+                <ul>
+                    <li><a href="../cart.php">Shopping Cart</a></li>
+                    <li><a href="../contact.php">Contact Us</a></li>
+                    <li><a href="../shop.php">Shop</a></li>
+                    <li><a href="my_account.php">My Account</a></li>
+                    
+                </ul>
+                <hr>
+                <h4>User Section</h4>
+                <ul>
+                <li><a href="../checkout.php">Login</a></li>
+                <li><a href="../customer_registration.php">Register</a></li>
+                </ul>
+                <hr class="hidden-md hidden-lg hidden-sm">
+            </div><!--col-md-3 col-sm-6 End-->
+            <div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 start-->
+            <h4>Top Product Categories</h4>
+            <ul>
+                <li><a href="#">Jacket</a></li>
+                <li><a href="#">Accessories</a></li>
+                <li><a href="#">Shoes</a></li>
+                <li><a href="#">Coats</a></li>
+                <li><a href="#">T-Shirts</a></li>
+            </ul>
+            <hr class="hidden-md hidden-lg">
+
+            </div><!--col-md-3 col-sm-6 End-->
+            <div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 start-->
+            <h4>Where to find us</h4>
+            <p>
+                <strong>teehosting.com</strong>
+                <br>Sohawal
+                <br>Ayodhya
+                <br>uttar pradesh
+                <br>icsrlabindia@gmail.com
+                <br>+91 9956494544
+            </p>
+            <a href="contact.php">GoTo contact us page</a>
+             <hr class="hidden-md hidden-lg">
+            </div><!--col-md-3 col-sm-6 End-->
+            <div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 start-->
+            <h4>Get the news</h4>
+            <p class="text-muted">
+                subscribe here for getting news of icsrlab ayodhya
+            </p>
+            <form action="" method="post">
+                <div class="input-group">
+                    <input type="text" name="email" class="form-control">
+                    <span class="input-group-btn">
+                        <input type="submit" class="btn btn-default" value="subscribe">
+                    </span>
+                </div></form>
+                <hr>
+                <h4>Stay In Touch<h4>
+                    <p class="social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                        <a href="#"><i class="fa fa-envelope"></i></a>
+                    </p>
+
+
+            </div><!--col-md-3 col-sm-6 End-->
+        </div>
+
+    </div>
+</div><!--Footer Section End-->
+
+<div id="copyright"><!--Copyright Section Start-->
+<div class="container">
+    <div class="col-md-6">
+        <p class="pull-left">
+           &copy:2021 B.Lasindu Dulanjana
+        </p>
+    </div>
+    <div class="col-md-6">
+        <p class="pull-right">
+            Template By:<a href="www.Teehosting.com">Teehosting.com</a>
+        </p>
+    </div>
+</div>
+
+</div><!--Copyright Section End-->
